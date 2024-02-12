8caf9e3c6ef0ff69ad4fb021ddd09fb4eae2d2d7
Aiko U <154068607+AikokulUlan-qa@users.noreply.github.com>
2024-02-12 21:11:58 +0300

    Update autotest.js

--- autotest.js
+++ autotest.js
@@ -4,12 +4,9 @@ pm.test("Status code is 200", function () {
     pm.response.to.have.status(200);
 });
 
-
-
 pm.test("создание покемона без имени(по тексту в боди)", function () {
     pm.expect(pm.response.text()).to.include("Отсутствует имя покемона");
 });
-
 
 
 pm.test("Response time is less than 50ms", function () {
@@ -17,12 +14,10 @@ pm.test("Response time is less than 50ms", function () {
 });
 
 
-
 pm.test("Проверяю id=17174", function () {
     var jsonData = pm.response.json();
     pm.expect(jsonData[0].id).to.eql("17174");
 });
-
 
 
 pm.test("Ожидаю статус 200", function () {
@@ -39,3 +34,31 @@ pm.collectionVariables.set("enemy_id", enemy_id); // Cохраним id прот
 });
 
 ```
+// autotest for method api.https://swapi.py4e.com
+```
+Request for Planet 7
+
+pm.test("Status code is 200", function () {
+    pm.response.to.have.status(200);
+});
+
+pm.test("Response time is less than 200ms", function () {
+    pm.expect(pm.response.responseTime).to.be.below(200);
+});
+
+pm.test("Planet name is correct", function () {
+    var jsonData = pm.response.json();
+    pm.expect(jsonData.name).to.eql("Endor");
+})
+
+Request for Starship 5
+
+pm.test("Response time is less than 210ms", function () {
+    pm.expect(pm.response.responseTime).to.be.below(200);
+});
+
+pm.test("Starship 5", function () {
+    pm.response.to.have.starship("5");
+});
+
+
